ba55b08b34700215f5e7c93b64cb314f9c283455
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2023-11-06 11:39:30 +0000

    Criando projeto com `typescript`

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express, { request, response } from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello Word" });
+});
+
+app.listen(3333);
